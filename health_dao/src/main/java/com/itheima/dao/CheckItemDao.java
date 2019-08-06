a81386ecad7a158a0b7538569471474126cfa521
Lunar <qwe151422709@hotmail.com>
2019-08-06 19:57:55 +0800

    传智健康项目checkitemadd实现

--- health_dao/src/main/java/com/itheima/dao/CheckItemDao.java
+++ health_dao/src/main/java/com/itheima/dao/CheckItemDao.java
@@ -1,8 +1,7 @@
 package com.itheima.dao;
 
 import com.itheima.pojo.CheckItem;
-import org.apache.ibatis.annotations.Param;
 
 public interface CheckItemDao {
-    void add(@Param("checkItem") CheckItem checkItem);
+    void add(CheckItem checkItem);
 }
--- health_service/src/main/java/com/itheima/service/CheckItemServiceImpl.java
+++ health_service/src/main/java/com/itheima/service/CheckItemServiceImpl.java
@@ -4,7 +4,10 @@ import com.alibaba.dubbo.config.annotation.Service;
 import com.itheima.dao.CheckItemDao;
 import com.itheima.pojo.CheckItem;
 import org.springframework.beans.factory.annotation.Autowired;
-@Service
+import org.springframework.transaction.annotation.Transactional;
+
+@Service(interfaceClass = CheckItemService.class)
+@Transactional
 public class CheckItemServiceImpl implements CheckItemService {
     @Autowired
     private CheckItemDao checkItemDao;
